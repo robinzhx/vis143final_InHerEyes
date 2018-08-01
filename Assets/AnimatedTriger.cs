252eb852d467dc81cf86640fb1803f6efac3a799
robinzhx <zhx068@ucsd.edu>
2018-08-01 07:18:13 -0700

    initial Update

---
+++ Assets/AnimatedTriger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTriger : MonoBehaviour {
+
+    public bool starttwist = true;
+    public float x = -0.005f, y = 0.0f, z = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (starttwist)
+        {
+            this.gameObject.transform.Rotate(new Vector3(x, y, z));
+        }
+	}
+}
---
+++ Assets/TeleportToObj.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TeleportToObj : MonoBehaviour {
+
+    public string checkName = "[CameraRig]";
+    public GameObject teleportToObj;
+    public GameObject CameraRigClone;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("entered collider " + col.gameObject.name);
+        if (col.gameObject.name == checkName && teleportToObj != null)
+        {
+            if (col.gameObject.name == "[CameraRig]")
+            {
+                col.gameObject.transform.position = teleportToObj.transform.position;
+            } else
+            {
+                GameObject.Find("[CameraRig]").transform.position = teleportToObj.transform.position;
+            }
+            
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
